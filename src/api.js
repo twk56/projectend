1f8d282b4d2441fc16641a0acfbedf674a2331d6
twk56 <thanawatklomkliew@gmail.com>
2025-03-26 00:28:13 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://api.iaaaiksu.com";
+const API_URL = "https://api.iaaaiksu.com/api";
 
 export const registerUser = async (userData) => {
     return await axios.post(`${API_URL}/register`, userData);
