e768ecbbf110188108e4c1c3b6641a16f4e84f59
twk56 <thanawatklomkliew@gmail.com>
2025-04-19 21:59:54 +0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://rest-std-api.onrender.com';
+const BASE_URL = 'https://rest-std-api.onrender.com/api';
 const STATIC_API_URL = 'https://rest-std-api.onrender.com';
 export default BASE_URL;
