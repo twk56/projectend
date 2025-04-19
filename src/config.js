b06f64dc2fd81f78baeb41c407ca81d5f600bccc
twk56 <thanawatklomkliew@gmail.com>
2025-04-19 22:15:16 +0700

    delpoy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://rest-std-api.onrender.com/api';
+const BASE_URL = 'https://rest-std-api.onrender.com';
 const STATIC_API_URL = 'https://rest-std-api.onrender.com';
 export default BASE_URL;
